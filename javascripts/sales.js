0f811fbe84b320fb43a3a42d150d2e4f0ec4ee1f
tendengango <framboutel@gmail.com>
2023-01-20 06:07:52 +0000

    soummission javascript

---
+++ javascripts/sales.js
@@ -0,0 +1,77 @@
+const products = [
+  {
+    id: 1,
+    name: "Mélange original 200g",
+    price: 500,
+  },
+  {
+    id: 2,
+    name: "Mélange original 500g",
+    price: 900,
+  },
+  {
+    id: 3,
+    name: "Mélange spécial 200g",
+    price: 700,
+  },
+  {
+    id: 4,
+    name: "Mélange spécial 500g",
+    price: 1200,
+  }
+]
+const priceElement = document.getElementById("product");
+const numberElement = document.getElementById("number");
+let purchases = [];
+function add() {
+    const targetId = parseInt(priceElement.value);
+    //const price = parseInt(priceElement.value);
+    const number = parseInt(numberElement.value);
+    const product = products.find(item => item.id == targetId);
+    let purchase = {
+        price: price,
+        number: number,
+      };
+      const newPurchase = purchases.findIndex((item) => item.product.id === purchase.product.id)
+      if(purchases.length < 1 || newPurchase === -1) {
+        purchases.push(purchase)
+      } else {
+        purchases[newPurchase].number += purchase.number
+      }
+      window.alert(`${display()}\nSous-Total${subtotal()}円`);
+      priceElement.value = "";
+      numberElement.value = "";
+}
+
+function subtotal() {
+  return purchases.reduce((prev, purchase)=> {
+    return prev + purchase.product.price * purchase.number;
+  }, 0);
+
+  
+}
+function display() {
+  return purchases.map(purchase => {
+    return `${purchase.product.name} ${purchase.product.price}円:${purchase.number}点\n`;
+  }).join("")
+};
+
+function calcPostageFromPurchase(sum) {
+  if (sum == 0 || sum >= 3000) {
+    return 0
+  } else if (sum < 1000){
+   return 500
+  } else {
+   return 250
+  }
+}
+function calc() {
+    
+    const sum = subtotal();
+    const postage = calcPostageFromPurchase(sum);
+    window.alert(`${display}\n${sum} Frais de livraison :${postage}円です。Total :${sum + postage}円です`);
+    purchases = [];
+    priceElement.value= "";
+    numberElement.value = "";  
+  }
+  
